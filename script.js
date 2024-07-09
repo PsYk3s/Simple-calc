b21ca300c0bdb3b18032782586aa6383570ddf5a
Warren <the.waz88@gmail.com>
2024-07-09 10:52:46 +0200

    big fixes to calculations

--- script.js
+++ script.js
@@ -9,8 +9,8 @@ equals.addEventListener("click", () => {
     let start = document.getElementById("start").valueAsDate;
     end = document.getElementById("end").valueAsDate;
 
-    const age = end.getFullYear() - start.getFullYear()
-    const finYear = start.getMonth() < 2 ? age - 1 : age;
+    const finYear = start.getMonth() < 2 ? end.getFullYear() - start.getFullYear():
+                    start.getMonth() >= 2 ? end.getFullYear() - start.getFullYear() -1: 0;
 
     display.innerText = `Tax age = ${finYear}`
     formula.innerText = `Birth date: ${start.toJSON().slice(0, 10)}`
